fa6f88116ba691fa1e9ac3513e0e5a54fc05c142
Edvinas Balčiūnas <edvinoske@inbox.lt>
2016-11-17 14:15:57 +0200

    Fixed LT characters, null handling

--- 2_Uzduotis/Program.cs
+++ 2_Uzduotis/Program.cs
@@ -11,34 +11,44 @@ namespace _2_Uzduotis
     {
         static void Main(string[] args)
         {
+           // System.Globalization.CultureInfo ci = new System.Globalization.CultureInfo("lt-LT");
+         //   System.Threading.Thread.CurrentThread.CurrentCulture = ci;
+
             string[] text = ReadFile("Duomenys.txt");
+            string[] original = new string[text.Length];
+
+            Array.Copy(text, original, text.Length);
+
             RemoveLongComment(ref text);
             RemoveRegularComments(ref text);
-            PrintToConsole(text);
+
+            PrintAnalysisToConsole(original, text);
             ResultsToFile("Rezultatai.txt", text);
+
             Console.ReadLine();
         }
         public static string[] ReadFile(string fName)
         {
-            string[] line = File.ReadAllLines(@fName);
-            return line; 
+            string[] line = File.ReadAllLines(@fName, Encoding.UTF8);
+            return line;
         }
         public static void ResultsToFile(string fName, string[] text)
         {
             using (StreamWriter writer = new StreamWriter(fName))
             {
                 foreach (string line in text)
-                   if (!string.IsNullOrWhiteSpace(line))
+                    if (!string.IsNullOrWhiteSpace(line))
                         writer.WriteLine(line);
             }
         }
+
         public static void RemoveRegularComments(ref string[] text)
         {
             for (int i = 0; i < text.Length; i++)
             {
-                if (text[i] != null)
+                string line = text[i];
+                if (!string.IsNullOrWhiteSpace(text[i]))
                 {
-                    string line = text[i];
                     for (int j = 0; j < line.Length - 1; j++)
                         if (line[j] == '/' && line[j + 1] == '/')
                         {
@@ -47,6 +57,7 @@ namespace _2_Uzduotis
                         }
                 }
             }
+            //RemoveNullStrings(ref text);
         }
         public static void RemoveLongComment(ref string[] text)
         {
@@ -61,36 +72,75 @@ namespace _2_Uzduotis
                 int endLineIndex = 0;
                 int endIndex = 0;
 
-                for (int j = 0; j < line.Length-1; j++)
-                    if (!inComment)
+                if (!string.IsNullOrWhiteSpace(line))
+                for (int j = 0; j < line.Length - 1; j++)
                     {
-                        if (line[j] == '/' && line[j + 1] == '*')
+                        if (!inComment)
                         {
-                            startLineIndex = i;
-                            startIndex = j;
-                            text[i] = line.Remove(startIndex);
-                            inComment = true;
-                        }
-                    }
-                    else
-                    {
-                        if (line[j] == '*' && line[j + 1] == '/')
-                        {
-                            endLineIndex = i;
-                            endIndex = j;
-                            text[i] = line.Remove(0,j+2);
-                            inComment = false;
+                            if (line[j] == '/' && line[j + 1] == '*')
+                            {
+                                startLineIndex = i;
+                                text[i] = line.Remove(j);
+                                inComment = true;
+                            }
                         }
                         else
-                            text[i] = null;
+                        {
+                            if (line[j] == '*' && line[j + 1] == '/')
+                            {
+                                endLineIndex = i;
+                                endIndex = j;
+                                text[i] = line.Remove(0, j + 2);
+                                inComment = false;
+                            }
+                            else
+                                text[i] = null;
+                        }
                     }
             }
+            //RemoveNullStrings(ref text);
         }
+
+        public static void RemoveNullStrings(ref string[] text)
+        {
+            int notNullCount = StringsWithValues(text);
+            string[] newText = new string[notNullCount];
+            int counter = 0;
+            foreach(string line in text)
+                if (!string.IsNullOrWhiteSpace(line))
+                {
+                    newText[counter++] = line;
+                }
+            text = newText;
+        }
+        public static int StringsWithValues(string[] text)
+        {
+            int notNullCount = 0;
+            foreach (string line in text)
+                if (!string.IsNullOrWhiteSpace(line))
+                    notNullCount++;
+            return notNullCount;
+        }
+
         public static void PrintToConsole(string[] text)
         {
             foreach(string line in text)
             {
                 Console.WriteLine(line);
+            }
+        }
+        public static void PrintAnalysisToConsole(string[] original, string[] text)
+        {
+            for (int i = 0; i < original.Length; i++)
+            {
+                if (string.IsNullOrWhiteSpace(text[i]))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(original[i]);
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                }
+                else
+                    Console.WriteLine(text[i]);
             }
         }
     }
